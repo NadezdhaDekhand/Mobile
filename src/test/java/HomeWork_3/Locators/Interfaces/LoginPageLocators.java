7bd65630202dfd25c62f2dd464e00e9d660b104f
NadezdhaDekhand <nadeshka7979@gmail.com>
2022-04-03 19:35:49 +0700

    Дз_5

---
+++ src/test/java/HomeWork_3/Locators/Interfaces/LoginPageLocators.java
@@ -0,0 +1,14 @@
+package HomeWork_3.Locators.Interfaces;
+
+import org.openqa.selenium.By;
+
+public interface LoginPageLocators {
+    By loginButton();
+    By loginErrorText();
+    By emailButton();
+    By passwordButton();
+    By repeatPasswordButton();
+    By signUpContainer();
+    By signUpButton();
+    By signUpErrorText();
+}
